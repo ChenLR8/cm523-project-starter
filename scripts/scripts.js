835d1f7b711ded431d1c40e30d596f670524ac6c
Luoran Chen <lorachen@bu.edu>
2023-04-27 13:11:33 -0400

    wip

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,13 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
+const nextbtn = document.querySelectorAll('.start');
+const backbtn = document.querySelectorAll('.back');
+const question =Array.from(document.querySelectorAll('fieldset'));
+
+let beginning = 0;
+
+question.forEach((question, index) => {
+    if (index > 0){
+        question.style.display='none';
+    }
+});
+
