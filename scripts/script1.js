ee5515f9b3aead92ce191b858e501531e150d0c0
Luoran Chen <lorachen@bu.edu>
2023-05-06 03:55:09 -0400

    wip

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,10 +1,14 @@
-const stopbtn = document.querySelector('.stop');
-const bg =document.querySelector('.bgcontainer');
-
-stopbtn.addEventListener('click', stopMotion);
-
-function stopMotion() {
-    bg.style.animation = 'none';
-  };
+const stopbtn = document.getElementById('switch');
+const bg = document.querySelector('.bgcontainer');
 
 
+
+stopbtn.addEventListener('change', animationControl)
+
+function animationControl() {
+  if (stopbtn.checked) {
+    bg.style.animationPlayState = 'paused';
+  } else {
+    bg.style.animationPlayState = 'running';
+  }
+};
